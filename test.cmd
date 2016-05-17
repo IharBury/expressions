da1ee51970d589ae53a55c5e1fd5ad6bd8bdbe91
Ihar Bury <Ihar.Bury@gmail.com>
2016-05-17 20:47:22 +0200

    dotnet test added to test.cmd

--- test.cmd
+++ test.cmd
@@ -9,3 +9,6 @@ if "%1"=="" set configuration=Debug
 %USERPROFILE%\.nuget\packages\xunit.runner.console\2.1.0\tools\xunit.console.exe test\IharBury.Expressions.Tests\bin\%configuration%\net40-client\win7-x64\IharBury.Expressions.Tests.dll
 %USERPROFILE%\.nuget\packages\xunit.runner.console\2.1.0\tools\xunit.console.exe test\IharBury.Expressions.Tests\bin\%configuration%\net35\win7-x64\IharBury.Expressions.Tests.dll
 %USERPROFILE%\.nuget\packages\xunit.runner.console\2.1.0\tools\xunit.console.exe test\IharBury.Expressions.Tests\bin\%configuration%\net35-client\win7-x64\IharBury.Expressions.Tests.dll
+
+cd test\IharBury.Expressions.Tests
+dotnet test --runtime netcoreapp1.0 --framework netcoreapp1.0 --configuration %configuration%
